20a561bcef34fa6e481b67ce3e8da44056bb5986
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-26 22:16:08 +0200

    yes^3 😎

--- Index/Program.cs
+++ Index/Program.cs
@@ -7,11 +7,15 @@ namespace Index
     {
         static void Main(string[] args)
         {
+            // kooling välkomstmeddelande:
             System.Console.WriteLine("HEY! Write something and index it!");
             
+            // program:
             while (true)
             {
+                // instruktioner
                 System.Console.WriteLine("Write; 'input' to add | 'read' to search | 'view' to view list");
+                // hämtar in svar och fortsätter med rätt klassmetod, om inte, skriv felmeddelande
                 string text = Console.ReadLine();
                 if (text == "input")
                 {
@@ -30,35 +34,44 @@ namespace Index
                     System.Console.WriteLine("'" + text + "' does not exist as command");
                 }
                 
+                // vänta på användaren innan skärmen rensas
                 System.Console.WriteLine("\npress 'enter' to proceed");
                 Console.ReadLine();
                 Console.Clear();
             }
 
+            // skapar en ny instans av klassen "Library"
             void NewIndex()
             {
                 Library library = new Library();
             }
+            // skriver metatext som finns under nyckeln(index) användaren angivit
             void ReadIndex()
             {
                 System.Console.WriteLine("Write index of information:");
 
                 string index = Console.ReadLine();
                 System.Console.WriteLine("this was found under " + index + ":");
+                // skriv metatext för index:
                 System.Console.WriteLine(Library.List[index]);
             }
+
+            // metoden skriver ut en lista på objekt i dictionary-Library.List, om inte skriver ut felmeddelande
             void ReadList()
             {   
                 System.Console.WriteLine("Contents:");
 
+                // om list har 0 index:
                 if (Library.List.Count == 0)
                 {
                     System.Console.WriteLine(":wow, such empty:");
                 }
+                // annars hämta dictionary keys från Library.List.Keys in i parametern indexColl
                 else
                 {
                     Dictionary<string, string>.KeyCollection indexColl = Library.List.Keys;
 
+                    // för varje meta(meta texten i varje klass Library) skriv nyckel namn
                     foreach(string meta in indexColl)
                     {
                         Console.WriteLine(meta);  
@@ -74,6 +87,7 @@ namespace Index
 
         string index;
         string meta;
+        // skriver ut information && instruktioner
         public Library()
         {
             System.Console.WriteLine("Index Name:");
@@ -81,8 +95,10 @@ namespace Index
             System.Console.WriteLine("Metta Text:");
             meta = Console.ReadLine();
 
+            // lägger till instans i indexList
             indexList.Add(index, meta);
         }
+        // reutrnerar indexList
         static public Dictionary<string, string> List
         {
             get
